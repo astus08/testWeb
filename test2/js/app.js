d0bb6f97308e672b48657f9efc560d5c953fdf53
Guillaume <guillaume.simonrimbault@viacesi.fr>
2017-03-30 17:27:23 +0200

    sync

--- test2/js/app.js
+++ test2/js/app.js
@@ -1,22 +1,23 @@
-$(document).ready(function() {
-    $('#country').keyup(function() {
-        var query = $(this).val();
-        if (query != ''){
-            $.ajax({
-                url: "search.php",
-                method:"POST",
-                data:{query:query},
-                success:function(data){
-                    $('#countryList').fadeIn();
-                    $('#countryList').html(data);
-                }
-            })
-        }
-    });
+// $(document).ready(function() {
+//     $('#country').keyup(function() {
+//         var query = $(this).val();
+//         if (query != ''){
+//             $.ajax({
+//                 url: "search.php",
+//                 method:"POST",
+//                 data:{query:query},
+//                 success:function(data){
+//                     $('#countryList').fadeIn();
+//                     $('#countryList').html(data);
+//                 }
+//             })
+//         }
+//     });
 
-    $(document).on('click', 'li', function(){
-        console.log('salut');
-        $("#country").val($(this).text());
-        $('#countryList').fadeOut();
-    });
-});
+//     $(document).on('click', 'li', function(){
+//         console.log('salut');
+//         $("#country").val($(this).text());
+//         $('#countryList').fadeOut();
+//     });
+// });
+
