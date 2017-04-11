3dc41d14f311a9077e464f6d4c67a62ec88a8619
Guillaume <guilllaume.simonrimbault@viacesi.fr>
2017-04-11 22:42:33 +0200

    sync

--- WS/gulpfile.js
+++ WS/gulpfile.js
@@ -3,30 +3,33 @@ $               = require('gulp-load-plugins')();
 
 browserSync     = require('browser-sync').create();
 
-var path_css    = 'css/';
-var path_scss   = 'css/';
+var path = {
+    css     : 'css/',
+    scss    : 'css/'
+}
 
-gulp.task('browserReload', function(done) {
+gulp.task('browser_reload', function(done) {
     browserSync.reload();
+    console.log('Restarted');
     done();
 });
 
-gulp.task('sass', function(){
-    gulp.src(path_scss + '*.scss')
+gulp.task('sass_compilation_+_prefixer', function(){
+    gulp.src(path.scss + '*.scss')
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.autoprefixer({
         browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest(path_css));
+    .pipe(gulp.dest(path.css));
 })
 
 
 gulp.task('default', function(){
     browserSync.init({
-        proxy: "localhost:8080/testWeb/WS"
+        proxy: "localhost:8080/Projets/testWeb/WS"
     });
 
-    gulp.watch(path_scss + '**/*.scss', ['sass'])
+    gulp.watch(path.scss + '**/*.scss', ['sass_compilation_+_prefixer'])
 
-    gulp.watch("*.html", ['browserReload']);
+    gulp.watch(['*.html', 'js/*.js', '*.php', 'css/*.css'], ['browser_reload']);
 })
